4164362d7626eb48920433cd0e0269264667d2c7
PocketBloom <bobelisabeth10@gmail.com>
2026-04-27 10:37:39 +0200

    Update Test Code - First Example

--- test.py
+++ test.py
@@ -1,1 +1,165 @@
 print("Hello There")
+
+import tudatpy
+
+# Load standard modules
+import numpy as np
+#from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')  # or try 'QtAgg'
+import matplotlib.pyplot as plt
+
+# Load tudatpy modules
+from tudatpy.interface import spice
+from tudatpy import dynamics
+from tudatpy.dynamics import environment_setup, propagation_setup, simulator
+from tudatpy.astro import element_conversion
+from tudatpy import constants
+from tudatpy.util import result2array
+from tudatpy.astro.time_representation import DateTime
+
+
+# Load spice kernels
+spice.load_standard_kernels()
+
+# Set simulation start and end epochs
+# So here end of the epoch is a day later :)
+# The start day is set to be 1st of January 2000 
+simulation_start_epoch = DateTime(2000, 1, 1).to_epoch()
+simulation_end_epoch   = DateTime(2000, 1, 2).to_epoch()
+
+
+
+# Create default body settings for "Earth"
+bodies_to_create = ["Earth"]
+
+# Create default body settings for bodies_to_create, with "Earth"/"J2000" as the global frame origin 
+# and orientation
+# So, we choose to consider the body of Earth
+
+global_frame_origin = "Earth"
+# I believe we always choose this
+global_frame_orientation = "J2000"
+body_settings = environment_setup.get_default_body_settings(
+    bodies_to_create, global_frame_origin, global_frame_orientation)
+
+
+plt.plot([1,2,3],[4,5,6])
+plt.show()
+
+#print("Follow up test, to see if Python runs the whole thing")
+
+# Add empty settings to body settings
+# Create the massless satellite for which the orbit around Earth will be propagated
+body_settings.add_empty_settings("Delfi-C3")
+
+# Create system of bodies (in this case only Earth)
+bodies = environment_setup.create_system_of_bodies(body_settings)
+
+#now we've created the envirnoment
+#now we define the setup
+
+#---
+# Indeed, the satellite rotates the central body
+
+# Define bodies that are propagated
+bodies_to_propagate = ["Delfi-C3"]
+
+# Define central bodies of propagation
+central_bodies = ["Earth"]
+
+
+# Acceleration model:
+
+# Define accelerations acting on Delfi-C3
+acceleration_settings_delfi_c3 = dict(
+    Earth=[propagation_setup.acceleration.point_mass_gravity()]
+)
+
+acceleration_settings = {"Delfi-C3": acceleration_settings_delfi_c3}
+
+# Create acceleration models
+acceleration_models = propagation_setup.create_acceleration_models(
+    bodies, acceleration_settings, bodies_to_propagate, central_bodies
+)
+
+
+#Initial state
+
+# Set initial conditions for the satellite that will be
+# propagated in this simulation. The initial conditions are given in
+# Keplerian elements and later on converted to Cartesian elements
+earth_gravitational_parameter = bodies.get("Earth").gravitational_parameter
+initial_state = element_conversion.keplerian_to_cartesian_elementwise(
+    gravitational_parameter=earth_gravitational_parameter,
+    semi_major_axis=6.99276221e+06,
+    eccentricity=4.03294322e-03,
+    inclination=1.71065169e+00,
+    argument_of_periapsis=1.31226971e+00,
+    longitude_of_ascending_node=3.82958313e-01,
+    true_anomaly=3.07018490e+00,
+)
+
+
+# Create termination settings
+termination_settings = propagation_setup.propagator.time_termination(simulation_end_epoch)
+
+# Create numerical integrator settings
+integrator_settings = propagation_setup.integrator.runge_kutta_fixed_step(
+    time_step = 10.0,
+    coefficient_set = propagation_setup.integrator.CoefficientSets.rk_4 )
+
+# Create propagation settings
+propagator_settings = propagation_setup.propagator.translational(
+    central_bodies,
+    acceleration_models,
+    bodies_to_propagate,
+    initial_state,
+    simulation_start_epoch,
+    integrator_settings,
+    termination_settings
+)
+
+
+
+# Create simulation object and propagate the dynamics
+dynamics_simulator = simulator.create_dynamics_simulator(
+    bodies, propagator_settings
+)
+# Extract the resulting state history and convert it to an ndarray
+states = dynamics_simulator.propagation_results.state_history
+states_array = result2array(states)
+
+#-------------
+# Printing the results :)
+
+print(
+    f"""
+Single Earth-Orbiting Satellite Example.
+The initial position vector of Delfi-C3 is [km]: \n{
+    states[simulation_start_epoch][:3] / 1E3}
+The initial velocity vector of Delfi-C3 is [km/s]: \n{
+    states[simulation_start_epoch][3:] / 1E3}
+\nAfter {simulation_end_epoch} seconds the position vector of Delfi-C3 is [km]: \n{
+    states[simulation_end_epoch][:3] / 1E3}
+And the velocity vector of Delfi-C3 is [km/s]: \n{
+    states[simulation_end_epoch][3:] / 1E3}
+    """
+)
+
+# Define a 3D figure using pyplot
+fig = plt.figure(figsize=(6,6), dpi=125)
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title(f'Delfi-C3 trajectory around Earth')
+
+# Plot the positional state history
+ax.plot(states_array[:, 1], states_array[:, 2], states_array[:, 3], label=bodies_to_propagate[0], linestyle='-.')
+ax.scatter(0.0, 0.0, 0.0, label="Earth", marker='o', color='blue')
+
+# Add the legend and labels, then show the plot
+ax.legend()
+ax.set_xlabel('x [m]')
+ax.set_ylabel('y [m]')
+ax.set_zlabel('z [m]')
+ax.set_aspect('equal')
+plt.show()
