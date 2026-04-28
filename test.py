57c1c46c2f3933b0dc364bb4fab42a383cf76fa4
PocketBloom <bobelisabeth10@gmail.com>
2026-04-28 13:40:00 +0200

    Update with TU3 for Earth

--- test.py
+++ test.py
@@ -19,6 +19,9 @@ from tudatpy.util import result2array
 from tudatpy.astro.time_representation import DateTime
 
 
+#FIRST EXAMPLE
+
+
 # Load spice kernels
 spice.load_standard_kernels()
 
@@ -43,9 +46,6 @@ global_frame_orientation = "J2000"
 body_settings = environment_setup.get_default_body_settings(
     bodies_to_create, global_frame_origin, global_frame_orientation)
 
-
-plt.plot([1,2,3],[4,5,6])
-plt.show()
 
 #print("Follow up test, to see if Python runs the whole thing")
 
@@ -162,4 +162,101 @@ ax.set_xlabel('x [m]')
 ax.set_ylabel('y [m]')
 ax.set_zlabel('z [m]')
 ax.set_aspect('equal')
-plt.show()
+#plt.show()
+
+
+#ME TRYING TO FIND TU3
+
+#I assume that I load the spice kernels and use the same start and stop time
+
+
+# Create default body settings for "Earth"
+bodies_to_create_TU3 = ["Earth"]
+
+# Create default body settings for bodies_to_create, with "Earth"/"J2000" as the global frame origin 
+# and orientation
+# So, we choose to consider the body of Earth
+
+global_frame_origin_TU3 = "Earth"
+# I believe we always choose this
+global_frame_orientation_TU3 = "J2000"
+
+body_settings_TU3 = environment_setup.get_default_body_settings(
+    bodies_to_create_TU3, global_frame_origin_TU3, global_frame_orientation_TU3)
+
+# Add empty settings to body settings
+# Create the massless satellite for which the orbit around Earth will be propagated
+body_settings_TU3.add_empty_settings("TU3")
+
+# Create system of bodies (in this case only Earth)
+bodies_TU3 = environment_setup.create_system_of_bodies(body_settings_TU3)
+
+# Define bodies that are propagated
+bodies_to_propagate_TU3 = ["TU3-1998"]
+
+# Define central bodies of propagation
+central_bodies_TU3 = ["Earth"]
+
+
+# Acceleration model:
+
+# Define accelerations acting on Delfi-C3
+acceleration_settings_TU3 = dict(
+    Earth=[propagation_setup.acceleration.point_mass_gravity()]
+)
+
+acceleration_settings_TU3 = {"TU3-1998": acceleration_settings_TU3}
+
+# Create acceleration models
+acceleration_models_TU3 = propagation_setup.create_acceleration_models(
+    bodies_TU3, acceleration_settings_TU3, bodies_to_propagate_TU3, central_bodies_TU3
+)
+
+#--------------------------------------------
+
+#Initial state
+
+# Set initial conditions for the satellite that will be
+# propagated in this simulation. The initial conditions are given in
+# Keplerian elements and later on converted to Cartesian elements
+
+# Let's just test what happens with the same initial state
+earth_gravitational_parameter_TU3 = bodies_TU3.get("Earth").gravitational_parameter
+initial_state_TU3 = element_conversion.keplerian_to_cartesian_elementwise(
+    gravitational_parameter_TU3=earth_gravitational_parameter,
+    semi_major_axis_TU3=6.99276221e+06,
+    eccentricity_TU3=4.03294322e-03,
+    inclination_TU3=1.71065169e+00,
+    argument_of_periapsis_TU3=1.31226971e+00,
+    longitude_of_ascending_node_TU3=3.82958313e-01,
+    true_anomaly_TU3=3.07018490e+00,
+)
+
+
+# Create termination settings
+termination_settings_TU3 = propagation_setup.propagator.time_termination(simulation_end_epoch)
+
+# Create numerical integrator settings
+integrator_settings_TU3 = propagation_setup.integrator.runge_kutta_fixed_step(
+    time_step_TU3 = 10.0,
+    coefficient_set_TU3 = propagation_setup.integrator.CoefficientSets.rk_4 )
+
+# Create propagation settings
+propagator_settings_TU3 = propagation_setup.propagator.translational(
+    central_bodies_TU3,
+    acceleration_models_TU3,
+    bodies_to_propagate_TU3,
+    initial_state_TU3,
+    simulation_start_epoch,
+    integrator_settings_TU3,
+    termination_settings_TU3
+)
+
+# Create simulation object and propagate the dynamics
+dynamics_simulator = simulator.create_dynamics_simulator(
+    bodies, propagator_settings
+)
+# Extract the resulting state history and convert it to an ndarray
+states = dynamics_simulator.propagation_results.state_history
+states_array = result2array(states)
+
